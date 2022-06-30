101af46defecb73f5a9656180d76b4e738fb3c4c
Alessandra Avelino <80168478+alessandraavelino@users.noreply.github.com>
2022-06-30 19:31:38 -0300

    Add files via upload

---
+++ html/index.js
@@ -0,0 +1,116 @@
+let btnEquipe = document.getElementById("equipe");
+let btnApostilas = document.getElementById("apostilas");
+let btnEventos = document.getElementById("eventos");
+let btnIndicacoes = document.getElementById("indicacoes");
+
+let modalEquipe = document.getElementById("modalEquipe");
+let atualStyleEquipe = modalEquipe.style.display;
+
+
+
+let list = document.querySelectorAll(".list");
+for (let i = 0; i< list.length; i++) {
+  list[i].onclick = function(){
+    let j = 0;
+    while(j < list.length){
+      list[j++].className = 'list';
+    }
+    list[i].className = 'active';
+  }
+}
+function abrirModalEquipe() {
+  
+  if (atualStyleEquipe != 'none') {
+    modalEquipe.style.display= 'block'
+    modalApostilas.style.display = 'none'
+    modalEventos.style.display = 'none'
+    modalIndicacoes.style.display = 'none'
+  }
+
+  else {
+
+  }
+}
+
+let modalApostilas = document.getElementById("modalApostilas");
+let atualStyleApostilas = modalApostilas.style.display
+function abrirModalApostilas(){
+
+  if (atualStyleApostilas != 'none'){
+    modalEquipe.style.display = 'none'
+    modalEventos.style.display = 'none'
+    modalIndicacoes.style.display = 'none'
+    modalApostilas.style.display= 'block'
+
+  } else {
+    
+  }
+}
+
+let modalEventos = document.getElementById("modalEventos")
+let atualStyleEventos = modalEventos.style.display
+
+function abrirModalEventos(){
+  if (atualStyleEventos != 'none'){
+    modalEquipe.style.display = 'none'
+    modalApostilas.style.display = 'none'
+    modalIndicacoes.style.display = 'none'
+    modalEventos.style.display = 'block'
+    
+  }
+}
+
+let modalIndicacoes = document.getElementById("modalIndicacoes")
+let atualStyleIndicacoes = modalIndicacoes.style.display
+function abrirModalIndicacoes(){
+  if (atualStyleIndicacoes != 'none'){
+    modalEquipe.style.display = 'none'
+    modalApostilas.style.display = 'none'
+    modalEventos.style.display = 'none'
+    modalIndicacoes.style.display = 'block'
+  }
+}
+
+btnEquipe.addEventListener("click", abrirModalEquipe)
+window.onclick = function(event) {
+  // event.preventDefault()
+  const modalEquipe = document.getElementById("modalEquipe")
+  if (event.target == modalEquipe) {
+    abrirModalEquipe()
+    
+  } else {
+    
+  }
+}
+
+
+btnApostilas.addEventListener("click", abrirModalApostilas)
+window.onclick = function(event) {
+  // event.preventDefault()
+  const modalApostilas = document.getElementById("modalApostilas")
+  if (event.target == modalApostilas) {
+    abrirModalApostilas()
+    
+  }
+}
+
+btnEventos.addEventListener("click", abrirModalEventos)
+window.onclick = function(event) {
+  // event.preventDefault()
+  const modalEventos = document.getElementById("modalEventos")
+  if (event.target == modalEventos){
+    abrirModalEventos()
+  }
+}
+
+btnIndicacoes.addEventListener("click", abrirModalIndicacoes)
+window.onclick = function (event){
+  const modalIndicacoes = document.getElementById("modalIndicacoes")
+  if (event.target == modalIndicacoes){
+    abrirModalIndicacoes()
+  }
+}
+
+
+
+
