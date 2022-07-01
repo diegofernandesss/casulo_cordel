6c8fb0d1c215a3e8af40786efcee2bec3db2a225
Alessandra Avelino <80168478+alessandraavelino@users.noreply.github.com>
2022-07-01 08:39:54 -0300

    Adicionando animações com jQuery

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
+//Definição das variáveis
 let btnEquipe = document.getElementById("equipe");
 let btnApostilas = document.getElementById("apostilas");
 let btnEventos = document.getElementById("eventos");
 let btnIndicacoes = document.getElementById("indicacoes");
 
-let modalEquipe = document.getElementById("modalEquipe");
-let atualStyleEquipe = modalEquipe.style.display;
 
-
-
+//Selecionando a lista de botões para deixar ativo ao clicar
 let list = document.querySelectorAll(".list");
 for (let i = 0; i< list.length; i++) {
   list[i].onclick = function(){
@@ -18,20 +16,32 @@ for (let i = 0; i< list.length; i++) {
     list[i].className = 'active';
   }
 }
-function abrirModalEquipe() {
-  
+
+//Função de abrir modal da Equipe
+let modalEquipe = document.getElementById("modalEquipe");
+let atualStyleEquipe = modalEquipe.style.display;
+function abrirModalEquipe() { 
   if (atualStyleEquipe != 'none') {
     modalEquipe.style.display= 'block'
     modalApostilas.style.display = 'none'
     modalEventos.style.display = 'none'
     modalIndicacoes.style.display = 'none'
   }
-
-  else {
-
-  }
+  $(".integrante").fadeOut(10).fadeIn(1000)
 }
 
+btnEquipe.addEventListener("click", abrirModalEquipe)
+window.onclick = function(event) {
+  // event.preventDefault()
+  const modalEquipe = document.getElementById("modalEquipe")
+  if (event.target == modalEquipe) {
+    abrirModalEquipe()
+    
+  } 
+}
+
+
+//Função de abrir modal das Apostilas
 let modalApostilas = document.getElementById("modalApostilas");
 let atualStyleApostilas = modalApostilas.style.display
 function abrirModalApostilas(){
@@ -42,47 +52,9 @@ function abrirModalApostilas(){
     modalIndicacoes.style.display = 'none'
     modalApostilas.style.display= 'block'
 
-  } else {
-    
-  }
+  } 
+  $(".apostila").fadeOut(10).fadeIn(1000)
 }
-
-let modalEventos = document.getElementById("modalEventos")
-let atualStyleEventos = modalEventos.style.display
-
-function abrirModalEventos(){
-  if (atualStyleEventos != 'none'){
-    modalEquipe.style.display = 'none'
-    modalApostilas.style.display = 'none'
-    modalIndicacoes.style.display = 'none'
-    modalEventos.style.display = 'block'
-    
-  }
-}
-
-let modalIndicacoes = document.getElementById("modalIndicacoes")
-let atualStyleIndicacoes = modalIndicacoes.style.display
-function abrirModalIndicacoes(){
-  if (atualStyleIndicacoes != 'none'){
-    modalEquipe.style.display = 'none'
-    modalApostilas.style.display = 'none'
-    modalEventos.style.display = 'none'
-    modalIndicacoes.style.display = 'block'
-  }
-}
-
-btnEquipe.addEventListener("click", abrirModalEquipe)
-window.onclick = function(event) {
-  // event.preventDefault()
-  const modalEquipe = document.getElementById("modalEquipe")
-  if (event.target == modalEquipe) {
-    abrirModalEquipe()
-    
-  } else {
-    
-  }
-}
-
 
 btnApostilas.addEventListener("click", abrirModalApostilas)
 window.onclick = function(event) {
@@ -94,6 +66,20 @@ window.onclick = function(event) {
   }
 }
 
+//Abrir modal de Eventos
+let modalEventos = document.getElementById("modalEventos")
+let atualStyleEventos = modalEventos.style.display
+function abrirModalEventos(){
+  if (atualStyleEventos != 'none'){
+    modalEquipe.style.display = 'none'
+    modalApostilas.style.display = 'none'
+    modalIndicacoes.style.display = 'none'
+    modalEventos.style.display = 'block'
+    
+  }
+  $(".imgEvento").fadeOut(10).fadeIn(1000)
+}
+
 btnEventos.addEventListener("click", abrirModalEventos)
 window.onclick = function(event) {
   // event.preventDefault()
@@ -101,6 +87,19 @@ window.onclick = function(event) {
   if (event.target == modalEventos){
     abrirModalEventos()
   }
+}
+
+//Abrir modal de Indicações
+let modalIndicacoes = document.getElementById("modalIndicacoes")
+let atualStyleIndicacoes = modalIndicacoes.style.display
+function abrirModalIndicacoes(){
+  if (atualStyleIndicacoes != 'none'){
+    modalEquipe.style.display = 'none'
+    modalApostilas.style.display = 'none'
+    modalEventos.style.display = 'none'
+    modalIndicacoes.style.display = 'block'
+  }
+  $(".imgIndicacao").fadeOut(10).fadeIn(1000)
 }
 
 btnIndicacoes.addEventListener("click", abrirModalIndicacoes)
